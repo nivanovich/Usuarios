a724b9fdc705b426bef5d200f8446529a75d655c
Nicolas <Nicolas@Animalitos-PC>
2021-11-10 14:33:08 -0300

    algunos test unitarios

--- registroUsuarios/src/main/java/com/evaluacion/ntt/dto/LoginRequest.java
+++ registroUsuarios/src/main/java/com/evaluacion/ntt/dto/LoginRequest.java
@@ -7,4 +7,9 @@ public class LoginRequest {
 	private String usuario;
 	private String pasword;
 	private String token;
+	public LoginRequest(String usuario, String pasword) {
+		super();
+		this.usuario = usuario;
+		this.pasword = pasword;
+	}
 }
--- registroUsuarios/src/test/java/com/evaluacion/ntt/UsuariosApplicationTests.java
+++ registroUsuarios/src/test/java/com/evaluacion/ntt/UsuariosApplicationTests.java
@@ -1,13 +1,33 @@
 package com.evaluacion.ntt;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.evaluacion.ntt.api.UsuarioController;
+import com.evaluacion.ntt.api.ValidationController;
+import com.evaluacion.ntt.dto.LoginRequest;
+import com.evaluacion.ntt.dto.LoginResponse;
 
 @SpringBootTest
 class UsuariosApplicationTests {
-
+	@Autowired
+	UsuarioController usuariocontroller;
+	@Autowired
+	ValidationController validacionController;
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws Exception {
+		assertThat(usuariocontroller).isNotNull();
+		assertThat(validacionController).isNotNull();
+	}
+	
+	@Test
+	public void loginTest() throws Exception {
+		LoginRequest loginReq=new LoginRequest("nivanovich","Hunter22");
+		LoginResponse loginRes=validacionController.validar(loginReq);
+		assertThat(loginRes.getToken()).isNotNull();
 	}
 
 }
